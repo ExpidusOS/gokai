7608cdbffe42743d8b89222f614d950bd9008218
Tristan Ross <tristan.ross@midstall.com>
2023-06-19 05:48:38 -0700

    chore(gokai): slim down linux build

--- packages/gokai/example/linux/main.cc
+++ packages/gokai/example/linux/main.cc
@@ -1,6 +1,3 @@
-#include <gokai/flutter/application.h>
-
 int main(int argc, char** argv) {
-  g_autoptr(GokaiApplication) app = gokai_application_new(APPLICATION_ID);
-  return g_application_run(G_APPLICATION(app), argc, argv);
+  return 0;
 }
--- packages/gokai/linux/include/gokai/gokai_plugin.h
+++ packages/gokai/linux/include/gokai/gokai_plugin.h
@@ -1,16 +1,1 @@
 #pragma once
-
-#include <flutter_linux/flutter_linux.h>
-#include <gokai/flutter/common.h>
-
-G_BEGIN_DECLS
-
-typedef struct _GokaiPlugin GokaiPlugin;
-typedef struct {
-  GObjectClass parent_class;
-} GokaiPluginClass;
-
-FLUTTER_PLUGIN_EXPORT GType gokai_plugin_get_type();
-FLUTTER_PLUGIN_EXPORT void gokai_plugin_register_with_registrar(FlPluginRegistrar* registrar);
-
-G_END_DECLS
