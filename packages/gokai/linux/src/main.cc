6838629d402913b827c46caf14a2ce55cdb59984
Tristan Ross <tristan.ross@midstall.com>
2023-06-25 09:34:43 -0700

    chore(gokai): set default logger to binary name

--- packages/gokai/linux/src/main.cc
+++ packages/gokai/linux/src/main.cc
@@ -3,10 +3,21 @@
 #include <gokai/context.h>
 #include <gokai/logging.h>
 #include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/cfg/env.h>
 #include <assert.h>
 
 extern "C" FLUTTER_PLUGIN_EXPORT int main(int argc, char** argv) {
+  auto out_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+  out_sink->set_level(spdlog::level::trace);
+
+  auto err_sink = std::make_shared<spdlog::sinks::stderr_color_sink_mt>();
+  err_sink->set_level(spdlog::level::err);
+
+  auto default_logger = std::shared_ptr<spdlog::logger>(new spdlog::logger(argv[0], { out_sink, err_sink }));
+  spdlog::register_logger(default_logger);
+  spdlog::set_default_logger(default_logger);
+
   spdlog::cfg::load_env_levels();
 
   auto manager = new Gokai::API::os::Linux::BinderManager(Gokai::ObjectArguments({}));
@@ -25,7 +36,7 @@ extern "C" FLUTTER_PLUGIN_EXPORT int main(int argc, char** argv) {
   auto factory = binder->getObjectFactory();
   auto logger = reinterpret_cast<Gokai::Logger*>(factory->createPointer(typeid(Gokai::Logger).name(), Gokai::ObjectArguments({})));
 
-  //try {
+  try {
     auto context = static_cast<Gokai::Context*>(factory->createPointer(typeid(Gokai::Context).name(), Gokai::ObjectArguments({
       { "logger", logger },
     })));
@@ -34,12 +45,12 @@ extern "C" FLUTTER_PLUGIN_EXPORT int main(int argc, char** argv) {
 
     uv_run(context->getLoop(), UV_RUN_DEFAULT);
     delete context;
-  /*} catch (const std::exception& ex) {
+  } catch (const std::exception& ex) {
     spdlog::critical("Caught exception: {}", ex.what());
     delete manager;
     spdlog::shutdown();
     return EXIT_FAILURE;
-  }*/
+  }
 
   delete manager;
   spdlog::shutdown();
