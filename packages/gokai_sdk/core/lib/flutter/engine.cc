58e1e43190a0b42001f3cc14a7e348fdfdbb110c
Tristan Ross <tristan.ross@midstall.com>
2023-07-26 08:20:55 -0700

    chore(gokai_sdk): offload platform message callback handling onto a different thread

--- packages/gokai_sdk/core/lib/flutter/engine.cc
+++ packages/gokai_sdk/core/lib/flutter/engine.cc
@@ -6,6 +6,14 @@
 
 #define TAG "Gokai::Flutter::Engine"
 
+struct PlatformChannelSubmissionEvent {
+  Gokai::Flutter::Engine* engine;
+  std::string channel;
+  std::vector<uint8_t> response;
+  const FlutterPlatformMessageResponseHandle* response_handle;
+  uv_async_t handle;
+};
+
 using namespace Gokai::Flutter;
 
 bool TaskRunnerComp::operator()(const EngineTask& a, const EngineTask& b) const {
@@ -48,68 +56,67 @@ void Engine::log_message_callback(const char* tag, const char* message, void* da
 void Engine::platform_message_callback(const FlutterPlatformMessage* message, void* data) {
   auto self = reinterpret_cast<Engine*>(data);
 
-  self->logger->debug("Receiving message on method channel \"{}\" for engine {}", message->channel, self->id.str());
+  auto send_response = [self, message](std::vector<uint8_t> response) {
+    auto event = new PlatformChannelSubmissionEvent();
+    event->engine = self;
+    event->channel = message->channel;
+    event->response = response;
+    event->response_handle = message->response_handle;
 
-  std::vector<uint8_t> msg(message->message, message->message + message->message_size);
+    uv_async_init(self->context->getLoop(), &event->handle, [](auto handle) {
+      auto event = reinterpret_cast<PlatformChannelSubmissionEvent*>((char*)(handle) - offsetof(PlatformChannelSubmissionEvent, handle));
+      auto result = FlutterEngineSendPlatformMessageResponse(
+        event->engine->value, event->response_handle, event->response.data(), event->response.size()
+      );
 
-  auto find = self->method_channel_handlers.find(message->channel);
-  if (find != self->method_channel_handlers.end()) {
-    for (auto func : find->second) {
-      auto msg_resp = func(msg);
-      if (msg_resp.size() > 0) {
-        auto result = FlutterEngineSendPlatformMessageResponse(
-          self->value, message->response_handle, msg_resp.data(), msg_resp.size()
-        );
+      if (result != kSuccess) {
+        event->engine->logger->error("Failed to send response for engine {} on service channel \"{}\": {}", event->engine->id.str(), event->channel, result);
+      }
+      delete event;
+    });
+  };
 
-        if (result != kSuccess) {
-          self->logger->error("Failed to send response for engine {} on service channel \"{}\": {}", self->id.str(), message->channel, result);
+  std::thread([self, message, send_response]() {
+    self->logger->debug("Receiving message on method channel \"{}\" for engine {}", message->channel, self->id.str());
+
+    std::vector<uint8_t> msg(message->message, message->message + message->message_size);
+
+    auto find = self->method_channel_handlers.find(message->channel);
+    if (find != self->method_channel_handlers.end()) {
+      for (auto func : find->second) {
+        auto msg_resp = func(msg);
+        if (msg_resp.size() > 0) {
+          send_response(msg_resp);
+          return;
         }
+      }
+    }
+
+    for (auto service_name : self->context->getSystemServiceNames()) {
+      auto service = self->context->getSystemService(service_name);
+
+      auto service_channel = service->getServiceChannel();
+      if (service_channel == nullptr) continue;
+      if (service_channel->getName().compare(message->channel) != 0) continue;
+
+      auto msg_resp = service_channel->receive(self->id, msg);
+      if (msg_resp.size() > 0) {
+        send_response(msg_resp);
         return;
       }
     }
-  }
 
-  for (auto service_name : self->context->getSystemServiceNames()) {
-    auto service = self->context->getSystemService(service_name);
-
-    auto service_channel = service->getServiceChannel();
-    if (service_channel == nullptr) continue;
-    if (service_channel->getName().compare(message->channel) != 0) continue;
-
-    auto msg_resp = service_channel->receive(self->id, msg);
-    if (msg_resp.size() > 0) {
-      auto result = FlutterEngineSendPlatformMessageResponse(
-        self->value, message->response_handle, msg_resp.data(), msg_resp.size()
-      );
-      if (result != kSuccess) {
-        self->logger->error("Failed to send response for engine {} on service channel \"{}\": {}", self->id.str(), message->channel, result);
+    if (strcmp(message->channel, "Gokai::Context") == 0) {
+      auto msg_resp = self->context->channelReceive(self->id, msg);
+      if (msg_resp.size() > 0) {
+        send_response(msg_resp);
+        return;
       }
-      return;
     }
-  }
 
-  if (strcmp(message->channel, "Gokai::Context") == 0) {
-    auto msg_resp = self->context->channelReceive(self->id, msg);
-    if (msg_resp.size() > 0) {
-      auto result = FlutterEngineSendPlatformMessageResponse(
-        self->value, message->response_handle, msg_resp.data(), msg_resp.size()
-      );
-      if (result != kSuccess) {
-        self->logger->error("Failed to send response for engine {} on service channel \"{}\": {}", self->id.str(), message->channel, result);
-      }
-      return;
-    }
-  }
-
-  self->logger->warn("A handler for the \"{}\" method channel on engine {} was not set, sending an empty response.", message->channel, self->id.str());
-
-  std::vector<uint8_t> resp;
-  auto result = FlutterEngineSendPlatformMessageResponse(
-    self->value, message->response_handle, resp.data(), resp.size()
-  );
-  if (result != kSuccess) {
-    self->logger->error("Failed to send response for engine {} on method channel \"{}\": {}", self->id.str(), message->channel, result);
-  }
+    self->logger->warn("A handler for the \"{}\" method channel on engine {} was not set, sending an empty response.", message->channel, self->id.str());
+    send_response(std::vector<uint8_t>());
+  }).detach();
 }
 
 Engine::Engine(Gokai::ObjectArguments arguments)
